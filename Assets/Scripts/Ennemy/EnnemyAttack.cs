3e6cf95d642d633ff953bbbd8daffc864538f9b3
Joelle Blais <joelleblais@gmail.com>
2022-03-14 14:35:09 -0400

    layout level2 termine

---
+++ Assets/Scripts/Ennemy/EnnemyAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyAttack : EnnemyBehaviour
+{
+    public Transform player, shootPos;
+    public float range, timeShoot, shootSpeed;
+    private float distToPlayer;
+    public GameObject bullet;
+    private bool canShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distToPlayer <= range)
+        {
+            if (canShoot)
+            {
+                Attack();
+                canShoot = false;
+            }
+        }
+
+    }
+
+    void Attack()
+    {
+        GameObject newBullet = Instantiate(bullet, shootPos.position, Quaternion.identity);
+        newBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(shootSpeed * Time.fixedDeltaTime, 0f);
+    }
+
+
+}
---
+++ Assets/Scripts/Ennemy/EnnemyOs.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyOs : MonoBehaviour
+{
+
+    public float damage;
+    public float dieTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountDownTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator CountDownTimer()
+    {
+        yield return new WaitForSeconds(dieTime);
+
+        Destroy(gameObject);
+    }
+}
